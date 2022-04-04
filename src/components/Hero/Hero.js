b63b68c41722f232231f87c44a8f5048fc0cd9f7
Phakorn Praditthamrong <iketu65@gmail.com>
2022-04-04 23:17:37 +0700

    Commit 2 (Done Projects Component)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        iKE's Portfolio
+      </SectionTitle>
+      {/* ** แก้ด้วย1 ** */}
+      <SectionText> 
+      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      </SectionText>
+      {/* ** แก้ด้วย2 ** */}
+      <Button onClick={() => window.location = 'https://www.javascript.com/'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
